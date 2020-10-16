0cd220aeedeae391b78b5ba9d4f05a40fa114e6f
eudoxos <eu@doxos.eu>
2020-10-16 16:39:44 +0200

    use longer types for literals; fix some prefixes

--- bytesize.hh
+++ bytesize.hh
@@ -20,16 +20,16 @@ namespace bytesize {
 			double d=std::stod(m[1].str());
 			size_t mult=1;
 			if(m[3]=="") mult=1;
-			else if(m[3]=="ki") mult=1L<<10;
-			else if(m[3]=="Mi") mult=1L<<20;
-			else if(m[3]=="Gi") mult=1L<<30;
-			else if(m[3]=="Ti") mult=1L<<40;
-			else if(m[3]=="Pi") mult=1L<<50;
-			else if(m[3]=="k") mult=1'000L;
-			else if(m[3]=="M") mult=1'000'000L;
-			else if(m[3]=="G") mult=1'000'000'000L;
-			else if(m[3]=="T") mult=1'000'000'000'000L;
-			else if(m[3]=="P") mult=1'000'000'000'000'000L;
+			else if(m[3]=="ki") mult=1LL<<10;
+			else if(m[3]=="Mi") mult=1LL<<20;
+			else if(m[3]=="Gi") mult=1LL<<30;
+			else if(m[3]=="Ti") mult=1LL<<40;
+			else if(m[3]=="Pi") mult=1LL<<50;
+			else if(m[3]=="k") mult=1'000LL;
+			else if(m[3]=="M") mult=1'000'000LL;
+			else if(m[3]=="G") mult=1'000'000'000LL;
+			else if(m[3]=="T") mult=1'000'000'000'000LL;
+			else if(m[3]=="P") mult=1'000'000'000'000'000LL;
 			else throw std::logic_error("Unhandled prefix '"+m[2].str()+"'.");
 			return bytesize(d*mult);
 		}
@@ -37,12 +37,12 @@ namespace bytesize {
 		std::string format() const {
 			std::ostringstream oss;
 			oss<<std::setprecision(3);
-			if(bytes<1'000L) oss<<bytes<<" B";
-			else if(bytes<1'000'000L) oss<<(bytes*1./1000L)<<" kB";
-			else if(bytes<1'000'000'000L) oss<<(bytes*1./1000'000L)<<" MB";
-			else if(bytes<1'000'000'000'000L) oss<<(bytes*1./1000'000'000L)<<" GB";
-			else if(bytes<1'000'000'000'000'000L) oss<<(bytes*1./1000'000'000'000L)<<" TB";
-			else oss<<(bytes*1./1000'000'000'000'000L)<<" PB";
+			if(bytes<1'000LL) oss<<bytes<<" B";
+			else if(bytes<1'000'000LL) oss<<(bytes*1./1000LL)<<" kB";
+			else if(bytes<1'000'000'000LL) oss<<(bytes*1./1000'000LL)<<" MB";
+			else if(bytes<1'000'000'000'000LL) oss<<(bytes*1./1000'000'000LL)<<" GB";
+			else if(bytes<1'000'000'000'000'000LL) oss<<(bytes*1./1000'000'000'000LL)<<" TB";
+			else oss<<(bytes*1./1000'000'000'000'000LL)<<" PB";
 			return oss.str();
 		}
 		// implicit conversion to size_t
@@ -57,27 +57,27 @@ namespace bytesize {
 		// bytes only with integer
 		inline bytesize operator"" _B(unsigned long long int num){ return bytesize(num); }
 		// floating-point numbers, like 5.5_kB
-		inline bytesize operator"" _kiB(long double num){ return bytesize((size_t)((1L<<10)*num)); }
-		inline bytesize operator"" _MiB(long double num){ return bytesize((size_t)((1L<<20)*num)); }
-		inline bytesize operator"" _GiB(long double num){ return bytesize((size_t)((1L<<30)*num)); }
-		inline bytesize operator"" _TiB(long double num){ return bytesize((size_t)((1L<<40)*num)); }
-		inline bytesize operator"" _PiB(long double num){ return bytesize((size_t)((1L<<50)*num)); }
-		inline bytesize operator"" _kB(long double num){ return bytesize((size_t)(1'000L*num)); }
-		inline bytesize operator"" _MB(long double num){ return bytesize((size_t)(1'000'000L*num)); }
-		inline bytesize operator"" _GB(long double num){ return bytesize((size_t)(1'000'000'000L*num)); }
-		inline bytesize operator"" _TB(long double num){ return bytesize((size_t)(1'000'000'000L*num)); }
-		inline bytesize operator"" _PB(long double num){ return bytesize((size_t)(1'000'000'000'000L*num)); }
+		inline bytesize operator"" _kiB(long double num){ return bytesize((size_t)((1LL<<10)*num)); }
+		inline bytesize operator"" _MiB(long double num){ return bytesize((size_t)((1LL<<20)*num)); }
+		inline bytesize operator"" _GiB(long double num){ return bytesize((size_t)((1LL<<30)*num)); }
+		inline bytesize operator"" _TiB(long double num){ return bytesize((size_t)((1LL<<40)*num)); }
+		inline bytesize operator"" _PiB(long double num){ return bytesize((size_t)((1LL<<50)*num)); }
+		inline bytesize operator"" _kB(long double num){ return bytesize((size_t)(1'000LL*num)); }
+		inline bytesize operator"" _MB(long double num){ return bytesize((size_t)(1'000'000LL*num)); }
+		inline bytesize operator"" _GB(long double num){ return bytesize((size_t)(1'000'000'000LL*num)); }
+		inline bytesize operator"" _TB(long double num){ return bytesize((size_t)(1'000'000'000'000LL*num)); }
+		inline bytesize operator"" _PB(long double num){ return bytesize((size_t)(1'000'000'000'000'000LL*num)); }
 		// repeated for integer literals so that e.g. 5_kB works
-		inline bytesize operator"" _kiB(unsigned long long int num){ return bytesize((size_t)((1L<<10)*num)); }
-		inline bytesize operator"" _MiB(unsigned long long int num){ return bytesize((size_t)((1L<<20)*num)); }
-		inline bytesize operator"" _GiB(unsigned long long int num){ return bytesize((size_t)((1L<<30)*num)); }
-		inline bytesize operator"" _TiB(unsigned long long int num){ return bytesize((size_t)((1L<<40)*num)); }
-		inline bytesize operator"" _PiB(unsigned long long int num){ return bytesize((size_t)((1L<<50)*num)); }
-		inline bytesize operator"" _kB(unsigned long long int num){ return bytesize((size_t)(1'000L*num)); }
-		inline bytesize operator"" _MB(unsigned long long int num){ return bytesize((size_t)(1'000'000L*num)); }
-		inline bytesize operator"" _GB(unsigned long long int num){ return bytesize((size_t)(1'000'000'000L*num)); }
-		inline bytesize operator"" _TB(unsigned long long int num){ return bytesize((size_t)(1'000'000'000L*num)); }
-		inline bytesize operator"" _PB(unsigned long long int num){ return bytesize((size_t)(1'000'000'000'000L*num)); }
+		inline bytesize operator"" _kiB(unsigned long long int num){ return bytesize((size_t)((1LL<<10)*num)); }
+		inline bytesize operator"" _MiB(unsigned long long int num){ return bytesize((size_t)((1LL<<20)*num)); }
+		inline bytesize operator"" _GiB(unsigned long long int num){ return bytesize((size_t)((1LL<<30)*num)); }
+		inline bytesize operator"" _TiB(unsigned long long int num){ return bytesize((size_t)((1LL<<40)*num)); }
+		inline bytesize operator"" _PiB(unsigned long long int num){ return bytesize((size_t)((1LL<<50)*num)); }
+		inline bytesize operator"" _kB(unsigned long long int num){ return bytesize((size_t)(1'000LL*num)); }
+		inline bytesize operator"" _MB(unsigned long long int num){ return bytesize((size_t)(1'000'000LL*num)); }
+		inline bytesize operator"" _GB(unsigned long long int num){ return bytesize((size_t)(1'000'000'000LL*num)); }
+		inline bytesize operator"" _TB(unsigned long long int num){ return bytesize((size_t)(1'000'000'000'000LL*num)); }
+		inline bytesize operator"" _PB(unsigned long long int num){ return bytesize((size_t)(1'000'000'000'000'000LL*num)); }
 	}
 }
 #define BYTESIZE_FMTLIB_FORMATTER \
@@ -86,4 +86,6 @@ namespace bytesize {
 	  template<typename ParseContext> constexpr auto parse(ParseContext &ctx) { return ctx.begin(); } \
 	  template<typename FormatContext> auto format(const bytesize::bytesize &bs, FormatContext &ctx) { return format_to(ctx.out(),"{}",bs.format()); } \
 	};
+
+
 
